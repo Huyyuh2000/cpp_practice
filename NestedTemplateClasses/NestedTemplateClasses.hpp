2fc93857f0d98ded78f5e10fba5cdd95e583408b
Nguyen Gia Huy (MS/EDA12-XC) <Huy.NguyenGia2@vn.bosch.com>
2023-05-14 17:34:29 +0700

    Nested Template Class and Making Class Iterable

---
+++ NestedTemplateClasses/NestedTemplateClasses.hpp
@@ -0,0 +1,128 @@
+/**
+ * @file NestedTemplateClasses.hpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-05-14
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef NESTEDTEMPLATECLASSES_H_
+#define NESTEDTEMPLATECLASSES_H_
+
+#include <iostream>
+
+using namespace std;
+
+namespace NestedTemplateClasses
+{
+template<class T>
+class RingBuffer
+{
+public:
+    class Iterator;
+
+public:
+    RingBuffer(unsigned int s) : _size(s), _index(0), value(NULL)
+    {
+        value = new T[_size];
+    }
+
+    ~RingBuffer()
+    {
+        delete [] value;
+    }
+
+    void Add(T e)
+    {
+        if (_index <= _size-1)
+        {
+            value[_index] = e;
+            _index++;
+        }
+        else
+        {
+            _index = 0;
+            value[_index] = e;
+        }
+    }
+
+    unsigned int size()
+    {
+        return _size;
+    }
+
+    T & get(unsigned int i)
+    {
+        return value[i];
+    }
+
+    Iterator begin()
+    {
+        return Iterator(0, *this);
+    }
+
+    Iterator end()
+    {
+        return Iterator(_size, *this);
+    }
+
+
+private:
+    unsigned int _size;
+    unsigned int _index;
+    T *value;
+};
+
+template<class T>
+class RingBuffer<T>::Iterator
+{
+private:
+    unsigned int _index;
+    RingBuffer &_RingBuffer;
+
+public:
+    Iterator(unsigned int index, RingBuffer &p_RingBuffer) : _index(index) , _RingBuffer(p_RingBuffer)
+    {
+
+    }
+
+    T &operator*()
+    {
+        return _RingBuffer.get(_index);
+    }
+
+    bool operator!=(const Iterator &other) const
+    {
+        if (_index == other._index)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    Iterator &operator++(int)
+    {
+        _index++;
+        return *this;
+    }
+
+    Iterator &operator++()
+    {
+        _index++;
+        return *this;
+    }
+
+    void print()
+    {
+        cout << "Hello from iterator" << endl;
+    }
+};
+}
+
+#endif
